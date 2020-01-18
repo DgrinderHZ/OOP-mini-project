eddf6bf233a5c426d9b779028e96b572535eea8c
DgrinderHZ <hassanzekkouri@gmail.com>
2020-01-18 01:04:24 +0100

    Update CompteEntreprise

--- compte_entreprise.h
+++ compte_entreprise.h
@@ -5,7 +5,33 @@ class CompteEntreprise: public Compte{
 
     vector<string> personneAutorises;
 public:
+    CompteEntreprise(vector<string> *perAuto){
+        personneAutorises = *perAuto;
+    }
     void afficher();
-    void saisir();
+    void saisir(vector<string> *liste);
 };
 
+
+void CompteEntreprise::saisir(vector<string> *liste){
+
+    cin >> *this;
+
+    if(liste != NULL){
+        cout << "Changement des personnes autorise en cours...\n";
+        personneAutorises = *liste;
+        cout << "Fin du Changement des personnes autorise!\n";
+        compteOuvert[id] = *this;
+    }
+
+}
+
+void CompteEntreprise::afficher(){
+
+        cout << *this;
+
+        cout << "\nListe des personnes autorises: >>> \n"<<endl;
+        for(int i = 0; i < personneAutorises.size(); i++)
+            cout << personneAutorises[i] << "\n";
+
+}
