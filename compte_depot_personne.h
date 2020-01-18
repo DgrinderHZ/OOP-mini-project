438c7fe4271afeb54cbeebcf3a29b8466a43423d
DgrinderHZ <hassanzekkouri@gmail.com>
2020-01-18 15:34:43 +0100

    Create CompteDepotPersonne

---
+++ compte_depot_personne.h
@@ -0,0 +1,45 @@
+#ifndef H_CDP
+#define H_CDP
+#include "compte_depot.h"
+#include "compte_personne.h"
+
+class CompteDepotPersonne: public CompteDepot, public ComptePersonne{
+
+public:
+
+    CompteDepotPersonne(float mn=100, float mx=12000, float ff=100, int valeurDec=30):
+        CompteDepot(mn, mx, ff), ComptePersonne(valeurDec){}
+
+    void afficher();
+    void saisir();
+
+};
+
+void CompteDepotPersonne::saisir(){
+
+    CompteDepot::saisir();
+    /// Partie personne
+    cout << "Si vous aimeriez changer les valeurs de:\
+             La valeurs du decouvert:  \
+             Tapez >>> 1, sinon tapez >>> 0!: \n";
+
+    int cc; cin >> cc;
+
+    if(cc == 1){
+        cout << "Donner La valeurs du decouvert: >>> ";
+        cin >> valeurDecouvert;
+        compteOuvert[id] = *this;
+    }
+
+}
+
+void CompteDepotPersonne::afficher(){
+    cout << *this;
+    cout << "\nLa valeurs minimale necessaire pour ouvrir un compte: >>> "<< minimale <<endl;
+    cout << "\nLa valeurs maximale autorise pour un compte depot: >>> "<< maximale <<endl;
+    cout << "\nLa valeur des frais de gestion: "<< fraisFermeture <<endl;
+
+    cout << "\nLa valeurs du decouvert: >>> "<< valeurDecouvert <<endl;
+
+}
+#endif // H_CDP
